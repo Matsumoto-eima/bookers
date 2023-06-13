ce0f5faa925714b9e012cf15a846673e93b834ed
Matsumoto-eima <135197614+Matsumoto-eima@users.noreply.github.com>
2023-06-13 18:30:08 +0000

    Fifth

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,13 @@ class BooksController < ApplicationController
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
+    if @book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :index
+  end
   end
   def index
     @books = Book.all
@@ -21,10 +26,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
+  else
+    render :edit
   end
-
+end
  def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
